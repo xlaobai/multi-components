5e4d7831b1cc932d0acb9cf428a7046943158950
xlaobailu <xlaobailu@xiaoe-tech.com>
2023-07-25 19:58:48 +0800

    已补充代理

--- build/vite.config.ts
+++ build/vite.config.ts
@@ -8,6 +8,27 @@ export default defineConfig(({ command, mode, ssrBuild }):any => {
   console.log("🚀 ~ file: vite.config.ts:6 ~ defineConfig ~ command:", command)
   
   return {
-    plugins: [uni()]
+    plugins: [uni()],
+    server: {
+      proxy: {
+        "/_alive": {
+          target: 'https://appAKLWLitn7978.h5.xiaoeknow.com',
+          changeOrigin: true,
+          followRedirects: true,// 重点在这里
+          configure: (proxy, _options) => {
+            proxy.on('proxyRes', (proxyRes, req, res) => {
+              console.log("🚀 ~ file: vite.config.ts:47 ~ proxy.on ~ res:", res)
+              // console.log("🚀 ~ file: vite.config.ts:47 ~ proxy.on ~ req:", req)
+              // console.log("🚀 ~ file: vite.config.ts:55 ~ proxy.on ~ proxyRes:", proxyRes)
+              //  res.removeHeader("Access-Control-Allow-Origin")
+              //  res.removeHeader("access-control-allow-origin")
+              //  res.setHeader("Access-Control-Allow-Origin", "*")
+               res.setHeader("set-cookie","ko_token=52f4d470e5bae507a210ad7fdc935fe7")
+              proxyRes.pipe(res)
+            });
+          }
+        }
+      },
+    }
   }
 });
--- src/components/layout.vue
+++ src/components/layout.vue
@@ -10,6 +10,19 @@
             return {
                 "userName":"foo"
             }
+        },
+        created() {
+            uni.request({
+                url: "_alive/v3/base_info",
+                data: {
+                    resource_id: "l_64be3dbee4b03e4b54de55b3",
+                    app_id: "appAKLWLitn7978",
+                    type: 12
+                },
+                success: (res) => {
+                    console.log(res.data);
+                }
+            }) 
         }
     }
 </script>
